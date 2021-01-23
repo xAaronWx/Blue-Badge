f79a24a85f6860a138c5a7d390969a373826a075
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-01-23 12:51:54 -0500

    edit & delete

--- 05-Node-Server/client/get-scripts.js
+++ 05-Node-Server/client/get-scripts.js
@@ -33,6 +33,51 @@ function displayMine() {
         header.textContent = "You haven't made any posts yet!";
         header.setAttribute("class", "noPosts");
       } else {
+        // creates the cards
+        for (i = 0; i < response.length; i++) {
+          let display = document.getElementById("journals");
+          let card = document.createElement("div");
+          let body = document.createElement("div");
+          let header = document.createElement("h5");
+          let subtitle = document.createElement("h6");
+          let para = document.createElement("p");
+          let editBtn = document.createElement("button");
+          let deleteBtn = document.createElement("button");
+
+          let current = response[i];
+          let title = current.title;
+          let date = current.date;
+          let entry = current.entry;
+
+          display.appendChild(card);
+          card.appendChild(body);
+          body.appendChild(header);
+          body.appendChild(subtitle);
+          body.appendChild(para);
+          body.appendChild(editBtn);
+          body.appendChild(deleteBtn);
+
+          card.setAttribute("id", current.id);
+          card.setAttribute("class", "card");
+          body.setAttribute("class", "card-body");
+          header.setAttribute("class", "card-title");
+          subtitle.setAttribute("class", "card-subtitle mb-2 test-muted");
+          para.setAttribute("class", "card-text");
+
+          editBtn.setAttribute("class", "btn btn-dark deleteBtn");
+          editBtn.setAttribute("type", "button");
+          editBtn.setAttribute("onclick", "editJournal(${current.id})");
+
+          deleteBtn.setAttribute("class", "btn btn-dark deleteBtn");
+          deleteBtn.setAttribute("type", "button");
+          deleteBtn.setAttribute("onclick", "deleteJournal(${current.id})");
+
+          header.textContent = title;
+          subtitle.textContent = date;
+          para.textContent = entry;
+          editBtn.textContent = "Edit";
+          deleteBtn.textContent = "Delete";
+        }
       }
     });
 }
